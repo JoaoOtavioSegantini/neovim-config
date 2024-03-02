62aa285d89e0c2cc0862f20038cbf75afc7e4558
João Otávio <joaotavioos@outlook.com>
2024-03-02 14:05:52 -0300

    feat: add ror setup ✨

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,14 +3,3 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-
---vim.g.rustaceanvim = function()
-	-- Update this path
-
-	--local cfg = require("rustaceanvim.config")
-	--return {
-	--	dap = {
-		--	adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-	--	},
-	--}
---end
