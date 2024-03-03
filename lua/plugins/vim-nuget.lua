6b4a7986bef94ad5861eb2d2a391c48f372d1967
João Otávio <joaotavioos@outlook.com>
2024-03-03 07:41:19 -0300

    feat: add dotnet nuget configuration ✨

---
+++ lua/plugins/vim-nuget.lua
@@ -0,0 +1,23 @@
+return {
+	"PasiBergman/cmp-nuget",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+--	config = function()
+	--	require("cmp-nuget").setup({})
+		--require("cmp").setup({
+--
+	--		sources = {
+		--		{
+			--		name = "nuget",
+				--	keyword_length = 0,
+	--			},
+--			},
+		--	formatting = {
+			--	source_names = {
+				--	nuget = "(NuGet)",
+			--	},
+		--	},
+	--	})
+--	end,
+}
